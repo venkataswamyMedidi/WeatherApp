0ce0f4974064a5dbfc4b37ce387d5d3cd4aec8de
Venkataswamy M <swamy8533@gmail.com>
2023-01-03 13:19:26 -0700

    added Https

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     e.preventDefault();
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
     );
     const response = await api_call.json();
     console.log(response);
